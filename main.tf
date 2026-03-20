002d356efbc9b7758497658dd5d2fc66435c771a
Craig Sloggett <craigsloggett@gmail.com>
2026-03-20 14:30:25 -0400

    chore: Update vault-enterprise module to v4.0.1 and AWS provider to ~> 6.0 (#8)
    * chore: Update vault-enterprise module to v4.0.1 and AWS provider to ~> 6.0
    
    * docs: Regenerate terraform-docs

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+data "aws_route53_zone" "vault" {
+  name = var.route53_zone_name
+}
+
 data "aws_ami" "hc_base" {
   most_recent = true
   owners      = ["888995627335"]
@@ -9,13 +13,13 @@ data "aws_ami" "hc_base" {
 }
 
 module "vault" {
-  source = "git::https://github.com/craigsloggett/terraform-aws-vault-enterprise?ref=v1.0.2"
+  source = "git::https://github.com/craigsloggett/terraform-aws-vault-enterprise?ref=v4.0.1"
 
-  project_name        = "vault-enterprise-1-21-4"
-  route53_zone_name   = var.route53_zone_name
-  vault_license       = var.vault_license
-  ec2_key_pair_name   = var.ec2_key_pair_name
-  ec2_instance_ami_id = data.aws_ami.hc_base.id
+  project_name      = "vault-enterprise-1-21-4"
+  route53_zone      = data.aws_route53_zone.vault
+  vault_license     = var.vault_license
+  ec2_key_pair_name = var.ec2_key_pair_name
+  ec2_ami           = data.aws_ami.hc_base
 
   nlb_internal            = false
   vault_api_allowed_cidrs = ["0.0.0.0/0"]
