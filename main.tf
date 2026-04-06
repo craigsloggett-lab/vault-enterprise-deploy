15b0335a8934e1615c19e47712e2d2d38e800ac6
Craig Sloggett <craigsloggett@gmail.com>
2026-04-06 17:29:10 -0400

    Bump the module version number

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ data "aws_ami" "selected" {
 
 module "vault" {
   # tflint-ignore: terraform_module_pinned_source
-  source = "git::https://github.com/craigsloggett/terraform-aws-vault-enterprise?ref=v4.3.2"
+  source = "git::https://github.com/craigsloggett/terraform-aws-vault-enterprise?ref=v4.3.4"
 
   project_name      = var.project_name
   route53_zone      = data.aws_route53_zone.vault
