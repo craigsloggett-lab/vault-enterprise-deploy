16afa4fea3db9481a18bdc209e2ef5310669c883
Craig Sloggett <craigsloggett@gmail.com>
2026-04-15 14:43:38 -0400

    Validate recent module changes

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ data "aws_ami" "selected" {
 
 module "vault" {
   # tflint-ignore: terraform_module_pinned_source
-  source = "git::https://github.com/craigsloggett/terraform-aws-vault-enterprise?ref=7d9679c28804e9fe6ee71a04bd6461628df95516"
+  source = "git::https://github.com/craigsloggett/terraform-aws-vault-enterprise?ref=885a1fc8750442dc022163b4c28152689be6e8a1"
 
   project_name             = var.project_name
   route53_zone             = data.aws_route53_zone.vault
