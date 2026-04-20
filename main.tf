486fdc196bb7854caa3d0d3042e2e9c415f56da8
Craig Sloggett <craigsloggett@gmail.com>
2026-04-20 12:55:20 -0400

    Deploy the latest module version

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ data "aws_ami" "selected" {
 }
 
 module "vault" {
-  source = "git::https://github.com/craigsloggett/terraform-aws-vault-enterprise?ref=v4.19.3"
+  source = "git::https://github.com/craigsloggett/terraform-aws-vault-enterprise?ref=v0.1.0"
 
   project_name             = var.project_name
   route53_zone             = data.aws_route53_zone.vault
