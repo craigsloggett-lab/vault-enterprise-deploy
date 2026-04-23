34c5325a9b2c4a8d3347d989f6b7a1ba23d012d0
Craig Sloggett <craigsloggett@gmail.com>
2026-04-23 15:45:24 -0400

    Remove CSR handshake until cloud-init is ready

--- pki.tf
+++ pki.tf
@@ -36,42 +36,42 @@ resource "tls_self_signed_cert" "root_ca" {
 
 # Intermediate CA Signing
 
-resource "terraform_data" "wait_for_csr" {
-  input = module.vault.intermediate_csr_ssm_parameter_name
+#resource "terraform_data" "wait_for_csr" {
+#  input = module.vault.intermediate_csr_ssm_parameter_name
+#
+#  provisioner "local-exec" {
+#    command = "${path.module}/files/wait-for-csr.sh"
+#    environment = {
+#      PARAMETER_NAME = self.input
+#      TIMEOUT_SEC    = "1800"
+#      REGION         = data.aws_region.current.region
+#    }
+#  }
+#}
 
-  provisioner "local-exec" {
-    command = "${path.module}/files/wait-for-csr.sh"
-    environment = {
-      PARAMETER_NAME = self.input
-      TIMEOUT_SEC    = "1800"
-      REGION         = data.aws_region.current.region
-    }
-  }
-}
+#data "aws_ssm_parameter" "intermediate_csr" {
+#  depends_on = [terraform_data.wait_for_csr]
+#  name       = module.vault.intermediate_csr_ssm_parameter_name
+#}
 
-data "aws_ssm_parameter" "intermediate_csr" {
-  depends_on = [terraform_data.wait_for_csr]
-  name       = module.vault.intermediate_csr_ssm_parameter_name
-}
-
-resource "tls_locally_signed_cert" "intermediate_ca" {
-  cert_request_pem   = data.aws_ssm_parameter.intermediate_csr.value
-  ca_private_key_pem = tls_private_key.root_ca.private_key_pem
-  ca_cert_pem        = tls_self_signed_cert.root_ca.cert_pem
-
-  validity_period_hours = 26280
-  is_ca_certificate     = true
-
-  allowed_uses = [
-    "cert_signing",
-    "crl_signing",
-  ]
-}
-
-resource "aws_secretsmanager_secret_version" "intermediate_ca" {
-  secret_id = module.vault.intermediate_ca_secret_arn
-  secret_string = jsonencode({
-    certificate = tls_locally_signed_cert.intermediate_ca.cert_pem
-    ca_chain    = tls_self_signed_cert.root_ca.cert_pem
-  })
-}
+#resource "tls_locally_signed_cert" "intermediate_ca" {
+#  cert_request_pem   = data.aws_ssm_parameter.intermediate_csr.value
+#  ca_private_key_pem = tls_private_key.root_ca.private_key_pem
+#  ca_cert_pem        = tls_self_signed_cert.root_ca.cert_pem
+#
+#  validity_period_hours = 26280
+#  is_ca_certificate     = true
+#
+#  allowed_uses = [
+#    "cert_signing",
+#    "crl_signing",
+#  ]
+#}
+#
+#resource "aws_secretsmanager_secret_version" "intermediate_ca" {
+#  secret_id = module.vault.intermediate_ca_secret_arn
+#  secret_string = jsonencode({
+#    certificate = tls_locally_signed_cert.intermediate_ca.cert_pem
+#    ca_chain    = tls_self_signed_cert.root_ca.cert_pem
+#  })
+#}
