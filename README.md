9fe298acbcc1f43c605e988404297cf5b616dbc5
Craig Sloggett <craigsloggett@gmail.com>
2026-06-12 06:17:22 -0400

    Bump the module version number

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ An infrastructure as code repository used to deploy a Vault Enterprise cluster t
 
 | Name | Source | Version |
 | ---- | ------ | ------- |
-| <a name="module_vault"></a> [vault](#module\_vault) | git::https://github.com/craigsloggett/terraform-aws-vault-enterprise | 5a120e8978361669cd40cb7e34311c0eec17fec6 |
+| <a name="module_vault"></a> [vault](#module\_vault) | git::https://github.com/craigsloggett/terraform-aws-vault-enterprise | v0.3.22 |
 
 ## Inputs
 
--- main.tf
+++ main.tf
@@ -52,8 +52,7 @@ data "aws_ssm_parameter" "vault_pki_ca_chain" {
 }
 
 module "vault" {
-  # tflint-ignore: terraform_module_pinned_source
-  source = "git::https://github.com/craigsloggett/terraform-aws-vault-enterprise?ref=5a120e8978361669cd40cb7e34311c0eec17fec6"
+  source = "git::https://github.com/craigsloggett/terraform-aws-vault-enterprise?ref=v0.3.22"
 
   vault_enterprise_license = var.vault_enterprise_license
   vault_fqdn               = "vault.${data.aws_route53_zone.vault.name}"
